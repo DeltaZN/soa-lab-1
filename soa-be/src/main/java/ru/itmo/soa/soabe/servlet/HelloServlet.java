1ec1c2ade52d267c494fc3dd014095faf51883b9
Georgii Savin <sawin.georgij@yandex.ru>
2021-08-14 21:51:40 +0300

    Moved cors logic to CorsFilter Removed jsp page

---
+++ soa-be/src/main/java/ru/itmo/soa/soabe/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package ru.itmo.soa.soabe.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("test");
+    }
+
+}
