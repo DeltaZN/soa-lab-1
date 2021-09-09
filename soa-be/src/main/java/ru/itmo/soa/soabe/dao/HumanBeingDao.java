c9656bd1b6a8e51f8280ede701ddb8ee90b3dfdf
Georgii Savin <gsavin@devexperts.com>
2021-09-09 10:50:28 +0300

    compile error fixed

--- soa-be/src/main/java/ru/itmo/soa/soabe/dao/HumanBeingDao.java
+++ soa-be/src/main/java/ru/itmo/soa/soabe/dao/HumanBeingDao.java
@@ -1,11 +1,7 @@
 package ru.itmo.soa.soabe.dao;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import ru.itmo.soa.soabe.converter.Converter;
-import ru.itmo.soa.soabe.datasource.HibernateDatasource;
 import ru.itmo.soa.soabe.entity.HumanBeing;
-import ru.itmo.soa.soabe.entity.TestEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,87 +14,33 @@ public class HumanBeingDao {
     }
 
     public List<HumanBeing> getAllHumans() {
-        Transaction transaction = null;
-        try (Session session = HibernateDatasource.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            TestEntity testEntity = new TestEntity();
-            session.save(testEntity);
-            transaction.commit();
-            return Optional.of(converter.toStr(testEntity));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        return Optional.empty();
+        return List.of();
     }
 
     public Optional<HumanBeing> getHuman(int id) {
-        Transaction transaction = null;
-        try (Session session = HibernateDatasource.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            TestEntity testEntity = new TestEntity();
-            session.save(testEntity);
-            transaction.commit();
-            return Optional.of(converter.toStr(testEntity));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
         return Optional.empty();
     }
 
     public Optional<HumanBeing> deleteHuman(int id) {
-        Transaction transaction = null;
-        try (Session session = HibernateDatasource.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            TestEntity testEntity = new TestEntity();
-            session.save(testEntity);
-            transaction.commit();
-            return Optional.of(converter.toStr(testEntity));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
         return Optional.empty();
     }
 
     public void createHuman(HumanBeing human) {
-        Transaction transaction = null;
-        try (Session session = HibernateDatasource.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            TestEntity testEntity = new TestEntity();
-            session.save(testEntity);
-            transaction.commit();
-            return Optional.of(converter.toStr(testEntity));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        return Optional.empty();
+//        Transaction transaction = null;
+//        try (Session session = HibernateDatasource.getSessionFactory().openSession()) {
+//            transaction = session.beginTransaction();
+//            TestEntity testEntity = new TestEntity();
+//            session.save(testEntity);
+//            transaction.commit();
+//            return Optional.of(converter.toStr(testEntity));
+//        } catch (Exception e) {
+//            if (transaction != null) {
+//                transaction.rollback();
+//            }
+//            e.printStackTrace();
+//        }
     }
 
     public void updateHuman(HumanBeing human) {
-        Transaction transaction = null;
-        try (Session session = HibernateDatasource.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            TestEntity testEntity = new TestEntity();
-            session.save(testEntity);
-            transaction.commit();
-            return Optional.of(converter.toStr(testEntity));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        return Optional.empty();
     }
 }
